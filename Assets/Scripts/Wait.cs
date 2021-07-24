bd0b5ebd2425291456b242732197d7774d0c6889
t8techboop <67003154+t8techboop@users.noreply.github.com>
2021-07-25 01:09:52 +0300

    added the folders from the other project

---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+
+    public float Wait_Time = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait_For_Intro());
+    }
+
+    IEnumerator Wait_For_Intro()
+    {
+        yield return new WaitForSeconds(Wait_Time);
+
+        SceneManager.LoadScene(1);
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Wait_Time = 0f;
+            Debug.Log("NullCorrect");
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    void update()
+    {
+
+    }
+
+}
